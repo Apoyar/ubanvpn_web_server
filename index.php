04fbbb68f6b2409eca8b570196a62fc4c084f90c
Sergey Andreev <sergey.adnreev@apoyara1.com>
2016-05-09 01:16:14 +0200

    uban vpn webs erver initial commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title><?php echo 'Hello World!'?></title>
+</head>
+<body>
+<h1><?php echo 'Hello World!'?></h1>
+<p><?php echo 'This text is rendered with PHP: '.phpversion()?></p>
+</body>
+</html>
